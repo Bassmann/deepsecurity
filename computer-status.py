fdcedf4c3db670f358a4f7854c5e7ca905e38e80
Martin Stemplinger <mstempl@gmail.com>
2019-07-13 15:37:21 +0200

    Moved API-Kex to properties.json

--- computer-status.py
+++ computer-status.py
@@ -1,32 +1,42 @@
-from __future__ import print_function
+import json
+import os
 import sys
 import warnings
-import deepsecurity
+import deepsecurity as api
 from deepsecurity.rest import ApiException
-from pprint import pprint
 
 # Setup
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
-configuration = deepsecurity.Configuration()
-configuration.host = 'https://app.deepsecurity.trendmicro.com/api'
 
-# Authentication
-configuration.api_key['api-secret-key'] = 'E93F360E-D8F0-AD7B-CA69-948C43CACB9E:3925613D-B360-AF16-A63F-952436C96350:RNE8zirCu3LWPhBMLUlBOG9jzgM0kHBrgxEeUiJIbaI='
+# Get the DSM URL and API key from a JSON file
+property_file = os.path.dirname(os.path.abspath(__file__)) + '/properties.json'
+print(property_file)
+
+with open(property_file) as raw_properties:
+    properties = json.load(raw_properties)
+
+secret_key = properties['secretkey']
+url = properties['url']
+api_version = 'v1'
+
+# Add DSM host information to the API client configuration
+configuration = api.Configuration()
+configuration.host = url
+configuration.api_key['api-secret-key'] = secret_key
 
 # Initialization
 # Set Any Required Values
-api_instance = deepsecurity.ComputersApi(deepsecurity.ApiClient(configuration))
-api_version = 'v1'
-expand_options = deepsecurity.Expand(deepsecurity.Expand.all_virtual_machine_summaries)
-# expand_options.add(deepsecurity.Expand.intrusion_prevention)
-# expand_options.add(deepsecurity.Expand.anti_malware)
+api_instance = api.ComputersApi(api.ApiClient(configuration))
+expand_options = api.Expand(api.Expand.all_virtual_machine_summaries)
+# expand_options.add(api.Expand.intrusion_prevention)
+# expand_options.add(api.Expand.anti_malware)
 expand = expand_options.list()
 overrides = False
 
 # Create a search filter with maximum returned items
-search_filter = deepsecurity.SearchFilter()
-search_filter.max_items = 1000
+search_filter = api.SearchFilter()
+search_filter.max_items = 10
 
 try:
     computers = api_instance.search_computers(api_version, search_filter=search_filter, expand=expand, overrides=overrides)
